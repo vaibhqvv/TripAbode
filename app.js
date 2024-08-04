6c3c2a75a4c0ac4e276d6b870cfe67097a2bbfbb
vaibhqvv <voidsyntaxes@gmail.com>
2024-08-04 23:58:05 +0530

    updated routes

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.get("/listings/:id/edit", async (req, res)=>{
 app.put("/listings/:id", async(req,res)=>{
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing });
-    res.redirect("/listings");
+    res.redirect(`/listings/${id}`);
 })
 
 
