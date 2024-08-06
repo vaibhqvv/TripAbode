4e96f77fdbf007dbbec50fb09b524f1d3a47ee79
vaibhqvv <voidsyntaxes@gmail.com>
2024-08-06 12:36:35 +0530

    Added DELETE Route

--- app.js
+++ app.js
@@ -71,6 +71,12 @@ app.put("/listings/:id", async(req,res)=>{
     res.redirect(`/listings/${id}`);
 })
 
+//Delete Route
+app.delete("/listings/:id", async(req, res)=>{
+    let {id} = req.params;
+    let deletedListing = await Listing. findByIdAndDelete(id);
+    res.redirect("/listings");
+})
 
 // app.get("/testListing", async (req, res)=>{
 //     let sampleListing = new Listing({
